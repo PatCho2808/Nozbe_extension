f25e825558d213e8e0ab125d565a9288b2a1b6b1
patrycja <patcho2808@gmail.com>
2020-08-04 10:20:41 +0200

    tasks added to lists

---
+++ main.js
@@ -0,0 +1,41 @@
+window.onload = function() {
+    let nozbe = new NozbeService();
+    nozbe.getTasks().then(
+        result => addTasksToLists(result),
+        error => console.log(error)
+    );
+}
+
+function addTasksToLists(tasks){
+    let todayTasks = document.getElementById('today_tasks');
+    let priorityTasks = document.getElementById('priority_tasks');
+    console.log(tasks);
+    tasks.forEach(task => {
+        if(isTaskDueToday(task))
+        {
+            addTaskToList(task, todayTasks);
+        }
+        else if(isTaskPriority(task))
+        {
+            addTaskToList(task, priorityTasks);
+        }
+    })
+}
+
+function isTaskDueToday(task){
+    let today = new Date();
+    let task_date = new Date(task.datetime);
+    return today.getDate() === task_date.getDate() &&
+        today.getMonth() === task_date.getMonth() &&
+        today.getFullYear() === task_date.getFullYear();
+}
+
+function isTaskPriority(task){
+    return task.next;
+}
+
+function addTaskToList(task, list){
+    let li = document.createElement('li');
+    li.innerHTML = task.name;
+    list.append(li);
+}
--- nozbeService.js
+++ nozbeService.js
@@ -1,19 +1,14 @@
 class NozbeService
 {
-    const CLIENT_ID = "5c96d2cd43a90d4f3a702f724e4b7d1380f68dbe";
-    const CLIENT_SECRET = "";
-    const ACCESS_TOKEN = ""
+    URL = 'https://api.nozbe.com:3000/';
 
-    async login()
-    {
-        const response = await fetch(
-            'https://api.nozbe.com:3000/oauth/secret/create',
-            { method: 'POST',
-                mode: 'no-cors',
-            headers: {
-                'Content-Type' : 'text/plain'},
-            body: "email=anastasia.pawlowicz@protonmail.com&password=s05kGgLJZk9L&redirect_uri=https://laaekoidklbijngehcedfcgbbhoiakgm.chromiumapp.org/newTab.html"
-            });
-        return response.text();
+    getToken(){
+        let json = JSON.parse(secrets);
+        return json[0]['access_token'];
+    }
+
+    async getTasks(){
+        const response = await fetch(`${this.URL}list?access_token=${this.getToken()}&type=task`);
+        return response.json();
     }
 }
